cab16cadc918c7e3121d824ee21dcb47254d4f6a
Erfan Gholizade <erfantkerfan@yahoo.com>
2020-04-19 18:17:27 +0430

    icon.gif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }}</title>
+    <link rel="icon" href="/image/icon.gif" type="image/gif">
 
     <!-- Scripts -->
     <script src="{{ asset('/js/app.js') }}" defer></script>
